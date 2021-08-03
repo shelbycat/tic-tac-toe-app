bc188076049bab27912a58a189a866f1ae326d83
Shelby Kauth <shelbykauth@gmail.com>
2021-08-02 21:43:26 -0400

    Prettify

--- packages/t3game/capacitor.config.ts
+++ packages/t3game/capacitor.config.ts
@@ -4,7 +4,7 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 't3game',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
